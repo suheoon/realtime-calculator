8e325c7b0705b5dc2255ff5d5a8aea9d6c0a9c93
Suheon Song <83093525+suheoon@users.noreply.github.com>
2023-03-16 16:28:43 +0900

    feat: currency 모델 수정

--- SmartCalculator/Models/Currency.swift
+++ SmartCalculator/Models/Currency.swift
@@ -12,7 +12,7 @@ typealias CurrencyArray = [Currency]
 let dict = ["OMR": "오만", "CLP": "칠레", "LKR": "스리랑카", "DZD": "알제리", "KES": "케냐", "COP": "콜롬비아", "TZS": "탄자니아", "NPR": "네팔", "RON": "루마니아", "LYD": "라비아", "MOP": "마카오", "MMK": "미얀마", "ETB": "에티오피아", "UZS": "우즈베키스탄", "KHR": "캄보디아", "FJD": "피지"]
 
 struct Currency: Codable {
-    let currencyCode, currencyName: String? // "USD" "달러" "미국"
+    let currencyCode, currencyName: String? // "USD" "달러"
     var country: String?
     let date, time: String?
     let basePrice, openingPrice, highPrice, lowPrice: Double?
@@ -28,6 +28,15 @@ struct Currency: Codable {
     let low52WDate: String?
     let currencyUnit: Int? // 화폐 단위
     let provider: String? // 하나은행
+    var changeRate: Double? {
+        guard let changePrice = changePrice, let basePrice = basePrice else { return 0.0 }
+        return changePrice / basePrice * 100
+    }
+    // country가 nil인 나라들의 이름
+    var countryName: String? {
+        guard let currencyCode = currencyCode else {return ""}
+        return dict[currencyCode]
+    }
     
     enum CodingKeys: String, CodingKey {
         case currencyCode, currencyName, country
@@ -47,14 +56,4 @@ struct Currency: Codable {
         case provider
     }
     
-    var changeRate: Double? {
-        guard let changePrice = changePrice, let basePrice = basePrice else { return 0.0 }
-        return changePrice / basePrice * 100
-    }
-    
-    // country가 nil일 경우
-    var countryName: String? {
-        guard let currencyCode = currencyCode else {return ""}
-        return dict[currencyCode]
-    }
 }
